9ccc09c6e169ee0389e9547f7fb5c5d513cbd323
RavenMQueen <48282684+RavenMQueen@users.noreply.github.com>
2025-01-08 14:13:20 +0100

    adding container to cases

--- app/lib/pages/debug.dart
+++ app/lib/pages/debug.dart
@@ -5,7 +5,6 @@ import 'package:coc/pages/nfc.dart';
 import 'package:coc/pages/login.dart';
 import 'package:coc/pages/evidence_list.dart';
 import 'package:coc/service/case.dart';
-import 'dart:math';
 
 class DebugPage extends StatelessWidget {
   const DebugPage({super.key});
@@ -76,42 +75,63 @@ class DebugPage extends StatelessWidget {
                 },
               ),
               const SizedBox(height: 20),
-              FutureBuilder<List<Case>>(
-                future: Case.fetchCases(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return Center(child: Text('Error occurred: ${snapshot.error}'));
-                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                    return const Center(child: Text('No evidence data found with current case ID'));
-                  } else {
-                    final caseList = snapshot.data!;
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: caseList.length,
-                      itemBuilder: (context, index) {
-                        final caseItem = caseList[index];
-                        return ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.all(10),
-                          ),
-                          onPressed: () {
-                            // TODO: Handle item click
-                            // log('Clicked on ${caseItem.caseID}');
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //     builder: (context) => EvidenceDetailView(evidenceItem: evidence),
-                            //   ),
-                            // );
-                          },
-                          child: Text("ID: ${caseItem.caseID}"),
-                        );
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[800],
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Case List',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    FutureBuilder<List<Case>>(
+                      future: Case.fetchCases(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return const Center(child: CircularProgressIndicator());
+                        } else if (snapshot.hasError) {
+                          return Center(child: Text('Error occurred: ${snapshot.error}'));
+                        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                          return const Center(child: Text('No cases found attached to you'));
+                        } else {
+                          final caseList = snapshot.data!;
+                          return ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: caseList.length,
+                            itemBuilder: (context, index) {
+                              final caseItem = caseList[index];
+                              return ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.all(10),
+                                ),
+                                onPressed: () {
+                                  // TODO: Handle item click
+                                  // log('Clicked on ${caseItem.caseID}');
+                                  // Navigator.push(
+                                  //   context,
+                                  //   MaterialPageRoute(
+                                  //     builder: (context) => EvidenceDetailView(evidenceItem: evidence),
+                                  //   ),
+                                  // );
+                                },
+                                child: Text("ID: ${caseItem.caseID}"),
+                              );
+                            },
+                          );
+                        }
                       },
-                    );
-                  }
-                },
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
