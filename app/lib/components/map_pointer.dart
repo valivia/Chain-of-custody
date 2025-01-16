f882f1aa69050d62e6caf163c04bb601d0a36dee
Zoë <zoe.vandijk@casema.nl>
2025-01-16 12:36:31 +0100

    🛠️ -- updated map widget -- wil polish it after lunch

--- app/lib/components/map_pointer.dart
+++ app/lib/components/map_pointer.dart
@@ -1,36 +1,76 @@
+import 'package:coc/controllers/tagged_evidence.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapPointer extends StatelessWidget {
-  final double latitude;
-  final double longitude;
+  final LatLng location;
 
-  const MapPointer({super.key, required this.latitude, required this.longitude});
+  const MapPointer({super.key, required this.location});
 
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
       options: MapOptions(
-        initialCenter: LatLng(latitude, longitude),
-        initialZoom: 5.0,
+        initialCenter: location,
+        initialZoom: 15.0,
       ),
       children: [
         TileLayer(
           urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-          subdomains: const['a', 'b', 'c'],
+          subdomains: const ['a', 'b', 'c'],
         ),
         MarkerLayer(
-          markers:  [
+          markers: [
             Marker(
-              point: LatLng(latitude, longitude),
+              point: location,
               height: 20,
-              width: 20, 
-              child: const Icon(Icons.location_on, color: Colors.red),              
+              width: 20,
+              child: const Icon(Icons.location_on, color: Colors.red),
             ),
           ],
         ),
       ],
+    );
+  }
+}
+
+class MapPointerBottomSheet extends StatelessWidget {
+  final TaggedEvidence evidenceItem;
+
+  const MapPointerBottomSheet({super.key, required this.evidenceItem});
+
+  @override
+  Widget build(BuildContext context) {
+    final location = evidenceItem.originCoordinates;
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            
+            Expanded(
+              child: MapPointer(location: location),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Origin location : (${location.latitude}, ${location.longitude})',
+              style: const TextStyle(fontSize: 16),
+            ),
+            
+            const SizedBox(height: 16),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Close'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
