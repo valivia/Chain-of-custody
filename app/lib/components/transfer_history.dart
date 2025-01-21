bbe0d5bca54f9501419e1c2c3da8b1061bc5ae4e
Zoë <zoe.vandijk@casema.nl>
2025-01-21 17:43:09 +0100

    🛠️-auditlog filtered on transfer

--- app/lib/components/transfer_history.dart
+++ app/lib/components/transfer_history.dart
@@ -1,23 +1,28 @@
 import 'package:coc/controllers/tagged_evidence.dart';
-import 'package:coc/controllers/audit_log.dart';
+import 'package:coc/controllers/audit_log.dart' as coc_audit;
 import 'package:flutter/material.dart';
 import 'dart:developer' as developer;
 
 class TransferHistoryView extends StatelessWidget {
   TransferHistoryView({super.key, required this.evidenceItem});
   final TaggedEvidence evidenceItem;
-  late List<AuditLog> auditLog;
+  late List<coc_audit.AuditLog> auditLog;
 
   gatherAuditLog() {
     return evidenceItem.auditLogs;
   }
 
 
+  List<coc_audit.AuditLog> gatherTransfers(auditLog){
+    return auditLog.where((log) => log.action == coc_audit.Action.transfer).toList();
+  }
+
 
 
   @override
   Widget build(BuildContext context) {
     auditLog = gatherAuditLog(); 
+    final tranferLog = gatherTransfers(auditLog);
 
 
     return Scaffold(
@@ -25,11 +30,11 @@ class TransferHistoryView extends StatelessWidget {
         title: const Text('Transfer History page'),
       ),
       body: ListView.builder(
-        itemCount: auditLog.length,
+        itemCount: tranferLog.length,
         itemBuilder: (context, index) {
-          final log = auditLog.elementAt(index);
+          final log = tranferLog.elementAt(index);
           return ListTile(
-          subtitle: Text('Date: ${log.id}, User: ${log.userId}, Action: ${log.action}'),
+          subtitle: Text('Date: ${log.id}, User: ${log.userId}, location: ${log.location}'),
           );
         },
       ),
