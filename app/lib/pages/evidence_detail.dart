66f581b0869bd8a24c6bfa1ad1bcdb463c4e3549
Valivia <valivia@xayania.com>
2025-01-27 18:40:44 +0100

    🛠️ Removed double padding

--- app/lib/pages/evidence_detail.dart
+++ app/lib/pages/evidence_detail.dart
@@ -30,85 +30,82 @@ class EvidenceDetailView extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Case ID: ${evidenceItem.caseId}',
-                style:
-                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Case ID: ${evidenceItem.caseId}',
+              style:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Evidence ID: ${evidenceItem.id}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Type: ${evidenceItem.itemType}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Description: ${evidenceItem.description}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Entry created at: ${formatTimestamp(evidenceItem.madeOn)}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            if (evidenceItem.updatedAt != null) ...[
               const SizedBox(height: 10),
               Text(
-                'Evidence ID: ${evidenceItem.id}',
+                'Entry updated at: ${formatTimestamp(evidenceItem.updatedAt!)}',
                 style: const TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                'Type: ${evidenceItem.itemType}',
-                style: const TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                'Description: ${evidenceItem.description}',
-                style: const TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                'Entry created at: ${formatTimestamp(evidenceItem.madeOn)}',
-                style: const TextStyle(fontSize: 16),
-              ),
-              if (evidenceItem.updatedAt != null) ...[
-                const SizedBox(height: 10),
-                Text(
-                  'Entry updated at: ${formatTimestamp(evidenceItem.updatedAt!)}',
-                  style: const TextStyle(fontSize: 16),
-                ),
-              ],
-              const SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    showModalBottomSheet(
-                      showDragHandle: true,
-                      context: context,
-                      builder: (BuildContext context) {
-                        return MapPointerBottomSheet(
-                            locationData: locationData[0], title: "Origin");
-                      },
-                    );
-                  },
-                  child: const Text('Origin location'),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[800],
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(left: 8.0),
-                      child: Text(
-                        'Transfers',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    LimTransferHistoryView(
-                        transfers: evidenceItem.transfers, itemCount: 2),
-                  ],
-                ),
               ),
             ],
-          ),
+            const SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                    showDragHandle: true,
+                    context: context,
+                    builder: (BuildContext context) {
+                      return MapPointerBottomSheet(
+                          locationData: locationData[0], title: "Origin");
+                    },
+                  );
+                },
+                child: const Text('Origin location'),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[800],
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      'Transfers',
+                      style: TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  LimTransferHistoryView(
+                      transfers: evidenceItem.transfers, itemCount: 2),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
