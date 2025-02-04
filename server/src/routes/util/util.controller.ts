2b510cd51ae47a6715551183f1e4ddc43d19fe2a
valivia <26413018+valivia@users.noreply.github.com>
2025-02-04 19:25:46 +0000

    ✨ Switched to nanoid

--- server/src/routes/util/util.controller.ts
+++ server/src/routes/util/util.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { createId } from '@paralleldrive/cuid2';
+import { nanoid } from "nanoid";
 import * as QRCode from 'qrcode'
 import { Public } from "src/guards/auth.guard";
 
@@ -10,7 +10,7 @@ export class UtilController {
   @Get("scan")
   @Public()
   async showScannable() {
-    const id = createId();
+    const id = nanoid(16);
     const code = await QRCode.toDataURL(id, { errorCorrectionLevel: 'H', width: 1000 });
     const html = `<img src="${code}" alt="QR Code for ${id}" />`;
     return html;
